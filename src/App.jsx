3f27cd9b3d8a2ff1e71ad8cb2f360698bac9e0b9
rohithchowdary2005 <rohithkodavati999@gmail.com>
2025-07-31 12:36:41 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S105</h1>
       <h1>CI/CD Cloud DeVops Our Course Instructor : Mr J SuryaKiran</h1>
       <p>This is a simple homepage deployed on GitHub Actions.</p>
+      <h1>Done with Git Pages & Git actions</h1>
     </div>
   );
 }
